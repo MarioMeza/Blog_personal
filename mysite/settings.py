b06522164603b31b6148a7458c833d65358fe992
mario.meza <uriel19961810@gmail.com>
2020-10-15 22:03:55 -0500

    add new configuration

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,7 +133,7 @@ MEDIA_URL = '/media/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
 # ]
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/media')
 PROJECT_DIR = os.path.dirname(__file__)
 TEMPLATE_DIRS = (os.path.join(PROJECT_DIR, '../templates'),)
 
